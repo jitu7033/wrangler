661b16d7e5f39de463f74fb5efa1c37451bcad37
jitu7033 <jraj41797@gmail.com>
2025-04-11 17:08:56 +0530

    Add BiteSize and Time_Duration classes and update TokenType and Usages definition

--- wrangler-api/src/main/java/io/cdap/wrangler/api/parser/TokenType.java
+++ wrangler-api/src/main/java/io/cdap/wrangler/api/parser/TokenType.java
@@ -152,5 +152,17 @@ public enum TokenType implements Serializable {
    * Represents the enumerated type for the object of type {@code String} with restrictions
    * on characters that can be present in a string.
    */
-  IDENTIFIER
+  IDENTIFIER,
+
+  /**
+   * Represent the enumerated type for the object of type {@code String} with restrictions
+   * on characters that can be present in a string.
+   */
+  BYTE_SIZE,
+
+  /*
+   * Represent the enumerated type for the object of type {@code String} with restrictions
+   * on characters that can be present in a string.
+   */
+  TIME_DURATION
 }
--- wrangler-api/src/main/java/io/cdap/wrangler/api/parser/UsageDefinition.java
+++ wrangler-api/src/main/java/io/cdap/wrangler/api/parser/UsageDefinition.java
@@ -127,6 +127,12 @@ public final class UsageDefinition implements Serializable {
         } else if (token.type().equals(TokenType.RANGES)) {
           sb.append("start:end=[bool|text|numeric][,start:end=[bool|text|numeric]*");
         }
+        else if(token.type().equals(TokenType.BYTE_SIZE)){
+          sb.append(token.name()).append(" <byte-size>");  // eg. "10MB"
+        }
+        else if (token.type().equals(TokenType.TIME_DURATION)) {
+          sb.append(token.name()).append(" <duration>");  // e.g., "150ms"
+        }
       }
 
       count--;
